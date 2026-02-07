ea4711133673864e0383891c789f01d78c44c042
Nekono Nana KAKKO KARI <github.agreed572@passmail.net>
2026-02-08 02:17:36 +0900

    fix: migrations/env.pyのapp contextエラーを修正、.dockerignoreを追加

--- migrations/env.py
+++ migrations/env.py
@@ -21,10 +21,6 @@ if database_url:
 if config.config_file_name is not None and os.path.exists(config.config_file_name):
     fileConfig(config.config_file_name)
 
-# Create app context so models are registered
-app = create_app()
-app.app_context().push()
-
 target_metadata = db.metadata
 
 
@@ -43,15 +39,18 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode."""
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
-        with context.begin_transaction():
-            context.run_migrations()
+    # Create app context so models are registered
+    app = create_app()
+    with app.app_context():
+        connectable = engine_from_config(
+            config.get_section(config.config_ini_section, {}),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
+        with connectable.connect() as connection:
+            context.configure(connection=connection, target_metadata=target_metadata)
+            with context.begin_transaction():
+                context.run_migrations()
 
 
 if context.is_offline_mode():
